68949e3781aee2b3f0090fdee7c3900c00f804ac
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-10-05 12:46:43 -0400

    Logged API Data from the server.

--- index.js
+++ index.js
@@ -8,16 +8,17 @@ const port = process.env.PORT || 3000;
 const API_KEY = process.env.KEY;
 
 app.listen(port, () => {
-    console.log(`Listening at port ${port}. `);
+    console.log(`Listening at port ${port}. `,);
+    // if (API_KEY == undefined) console.log("Undefined");
+    // else console.log("Is Defined.")
 })
 
-app.get('/', async (req, res) => {
+app.get('/api', async (req, res) => {
     try {
-        res.send('./index.html');
-        const API_URL = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=CNY&apikey=${API_KEY}`;
-        const data = await fetch(API_KEY);
-        const jsonData = data.json();
-        console.log(jsonData);
+        const API_URL = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${API_KEY}`;
+        const data = await fetch(API_URL);
+        const jsonData = await data.json();
+        // console.log(jsonData);
 
     }
     catch(e) {
